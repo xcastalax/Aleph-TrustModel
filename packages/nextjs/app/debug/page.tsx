31093dd8de9a31addb8339c34c57beddfde8abcb
xcastalax <castala.it@gmail.com>
2024-12-15 02:38:09 -0300

    adding modifications to the second page v1.0

--- packages/nextjs/app/debug/page.tsx
+++ packages/nextjs/app/debug/page.tsx
@@ -9,19 +9,13 @@ export const metadata = getMetadata({
 
 const Debug: NextPage = () => {
   return (
-    <>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-[#3B82F6] via-[#A855F7] to-[#EC4899]">
       <DebugContracts />
       <div className="text-center mt-8 bg-secondary p-10">
-        <h1 className="text-4xl my-0">Debug Contracts</h1>
-        <p className="text-neutral">
-          You can debug & interact with your deployed contracts here.
-          <br /> Check{" "}
-          <code className="italic bg-base-300 text-base font-bold [word-spacing:-0.5rem] px-1">
-            packages / nextjs / app / debug / page.tsx
-          </code>{" "}
-        </p>
+        <h1 className="text-4xl my-0 text-white">TrustModel</h1>
+        <p className="text-neutral">You can debug & interact with your deployed contracts here.</p>
       </div>
-    </>
+    </div>
   );
 };
 
